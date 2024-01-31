1dcac89103cb3f00f689a7c12e7c06aaea31af29
Devcomfortoj <comfortojo21@gmail.com.>
2024-02-01 03:02:19 +1400

    day2

---
+++ day5/main.js
@@ -0,0 +1,5 @@
+import web_techs from "./web_techs.js";
+import {countries} from "./countries.js";
+countries.includes("Ethopia")
+console.log(countries);
+console.log(web_techs);
---
+++ day5/web_techs.js
@@ -0,0 +1,15 @@
+export default[
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+]
+if(webTechs.includes('Sass')){
+    console.log("The web tech does exist");
+  }else{
+ webTechs.push("Sass")
+ console.log(webTechs);
+  }
